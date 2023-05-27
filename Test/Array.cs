9e62bd7331fa91803411f229423a675c168a6b51
rajaganapathi3076 <rajaganapathi3076@gmail.com>
2023-05-27 19:08:11 +0530

    adding 1

---
+++ Test/Array.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    internal class Array
+    {
+        public static void CheckArray()
+        {
+            int[] intArray;
+
+            // allocating memory for 5 integers.
+            intArray = new int[5];
+
+            // initialize the first elements
+            // of the array
+            intArray[0] = 10;
+
+            // initialize the second elements
+            // of the array
+            intArray[1] = 20;
+
+            // so on...
+            intArray[2] = 30;
+            intArray[3] = 40;
+            intArray[4] = 50;
+
+            // accessing the elements
+            // using for loop
+            Console.Write("For loop :");
+            for (int i = 0; i < intArray.Length; i++)
+                Console.Write(" " + intArray[i]);
+
+            Console.WriteLine("");
+            Console.Write("For-each loop :");
+
+            // using for-each loop
+            foreach (int i in intArray)
+                Console.Write(" " + i);
+
+            Console.WriteLine("");
+            Console.Write("while loop :");
+
+            // using while loop
+            int j = 0;
+            while (j < intArray.Length)
+            {
+                Console.Write(" " + intArray[j]);
+                j++;
+            }
+
+            Console.WriteLine("");
+            Console.Write("Do-while loop :");
+
+            // using do-while loop
+            int k = 0;
+            do
+            {
+                Console.Write(" " + intArray[k]);
+                k++;
+            } while (k < intArray.Length);
+
+        }    
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            int Cr, Pr, amount, Unit;
+            int rate;
+            Console.Write("Enter the current reading :");
+            Cr = int.Parse(Console.ReadLine());
+            Console.Write("Enter the previous reading :");
+            Pr = int.Parse(Console.ReadLine());
+            Unit = Math.Abs(Pr - Cr);
+            string type;
+            Console.Write("Enter the type :");
+            type = Console.ReadLine();
+
+
+
+            switch (type)
+            {
+
+
+                case "D":
+                    rate = 4;
+                    break;
+                case "C":
+                    rate = 10;
+                    break;
+                case "I":
+                    rate = 15;
+                    break;
+                default:
+                    Console.Write("Invalid input");
+                    rate = 0;
+                    break;
+
+            }
+            amount = Unit * rate;
+            Console.WriteLine("BillAmount:{0}", amount);
+        }
+}   }
+        
+           
+
+
+    
+
